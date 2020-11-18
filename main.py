8035cad5c4e7f74961eeea9d8691111ec27e36be
737898487 <737898487@qq.com>
2020-11-18 14:37:38 +0800

    main

---
+++ main.py
@@ -0,0 +1,26 @@
+from PRO.preprocess_main import preprocess
+from Re.bin import BinRe
+import os
+
+
+def del_file(path):
+    ls = os.listdir(path)
+    for i in ls:
+        c_path = os.path.join(path, i)
+        if os.path.isdir(c_path):
+            del_file(c_path)
+        else:
+            os.remove(c_path)
+
+
+if __name__ == "__main__":
+
+    data_path = input("Please input data_path:")
+    print(data_path)
+    if not os.path.exists("./cache"):
+        os.mkdir("./cache")
+    preprocess(data_path, "./cache")
+
+    BinRe("./cache")
+    del_file("./cache")
+    os.rmdir("./cache")
